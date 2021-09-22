5a1c7b0bd1ed80adfecce6a8c9ef08ee0c4c3f67
tripleloser <marvinIII@outlook.de>
2021-09-22 18:55:26 +0200

    Update week 3

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -12,7 +12,7 @@ export class ConfigService {
     return {
       minWeek: 1,
       maxWeek: 14,
-      currentWeek: 2,
+      currentWeek: 3,
       leagues: [
         {
           leagueId: '736264930570969088',
