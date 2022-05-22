e87436bd00f443129713fa189c8edd2d004b13a5
tripleloser <marvinIII@outlook.de>
2022-05-22 22:42:25 +0200

    Config

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -11,49 +11,34 @@ export class ConfigService {
   constructor() { }
 
   getCurrentWeek(): number {
-    const now = new Date();
-
-    if (now < new Date('2021-10-13'))
-      return 5;
-
-    if (now < new Date('2021-10-20'))
-      return 6;
-
-    if (now < new Date('2021-10-27'))
-      return 7;
-
-    if (now < new Date('2021-11-03'))
-      return 8;
-
-    if (now < new Date('2021-11-10'))
-      return 9;
-      
-    if (now < new Date('2021-11-17'))
-      return 10;
-
-    if (now < new Date('2021-11-24'))
-      return 11;
-
-    if (now < new Date('2021-12-01'))
-      return 12;
-
-    if (now < new Date('2021-12-08'))
-      return 13;
-
-    return 14;
+    return 1;
   }
 
   getLeaguesConfig(): Config {
     return {
       minWeek: 1,
-      maxWeek: 14,
+      maxWeek: 15,
       currentWeek: this.getCurrentWeek(),
       leagues: [
         {
           leagueId: '827607422108536832',
           relegationSettings: {
             playoffs: 0,
-            relegated: 1000,
+            relegated: 9,
+          }
+        },
+        {
+          leagueId: '827607422108536832',
+          relegationSettings: {
+            playoffs: 2,
+            relegated: 9,
+          }
+        },
+        {
+          leagueId: '827607422108536832',
+          relegationSettings: {
+            playoffs: 2,
+            relegated: 100000,
           }
         },
         // {
